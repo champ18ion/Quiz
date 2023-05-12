d96e05f8a4801ac84d3936f10429ca10c0a65dd5
Kunal Kumar <113764445+champ18ion@users.noreply.github.com>
2023-05-12 20:28:54 +0530

    normalize css

--- quiz-app/src/index.js
+++ quiz-app/src/index.js
@@ -1,5 +1,6 @@
 import React from 'react';
 import ReactDOM from 'react-dom/client';
+import 'normalize.css'
 import './index.css';
 import 'mdb-react-ui-kit/dist/css/mdb.min.css';
 import "@fortawesome/fontawesome-free/css/all.min.css";
